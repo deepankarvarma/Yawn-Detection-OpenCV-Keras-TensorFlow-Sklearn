05d759dad4f991aa850a0d24068f471c3efad23d
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-01 20:04:25 +0530

    added streamlit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import streamlit as st
 import cv2
 import numpy as np
@@ -22,22 +23,22 @@ def app():
 
     # Allow the user to upload an image
     uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
-
     if uploaded_file is not None:
         # Read the image
         img = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-
+        
         # Preprocess the image
         img = preprocess_image(img)
 
         # Make a prediction
         pred = model.predict(img)
-        pred_label = 'yawn' if pred[0][0] > 0.5 else 'no yawn'
+        pred_label = 'Yawning' if pred[0][0] > 0.5 else 'No Yawning'
         pred_prob = pred[0][0]
-
+        # Display the image
+        st.image(img, caption='Uploaded Image', use_column_width=True)
         # Show the prediction result
         st.write(f'Prediction: {pred_label}')
-        st.write(f'Probability: {pred_prob:.2f}')
+        st.write(f'Probability Of Yawning: {pred_prob:.2f}')
 
 # Run the app
 if __name__ == '__main__':
