b3c2ccb830be761a7d2efaecc673c818c75e8584
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-04 00:47:12 +0530

    minors

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import cv2
-from io import BytesIO
-
 import numpy as np
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from PIL import Image
 
 # Load the trained model
 model = load_model('yawn_detection_model.h5')
@@ -12,11 +12,16 @@ model = load_model('yawn_detection_model.h5')
 img_size = (224, 224)
 
 # Define a function to preprocess the input image
-def preprocess_image(img):
-    img = cv2.resize(img, img_size)
-    img = np.expand_dims(img, axis=0)
-    img = img / 255.0
-    return img
+def preprocess_image(image):
+    # Resize the image
+    image = image.resize(img_size)
+    # Convert the image to a NumPy array
+    image = img_to_array(image)
+    # Scale the pixel values to [0, 1]
+    image = image / 255.0
+    # Add an extra dimension to the array to represent the batch size
+    image = np.expand_dims(image, axis=0)
+    return image
 
 # Define the Streamlit app
 def app():
@@ -26,23 +31,15 @@ def app():
     uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
     if uploaded_file is not None:
         # Read the image
-        img = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-        # Encode the image in JPEG format.
-        ret, buffer = cv2.imencode('.jpg', img)
-
-        # Convert the encoded image data to a byte stream.
-        byte_stream = BytesIO(buffer)
-        
+        image = Image.open(uploaded_file)
         # Display the image
-        st.image(byte_stream, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
         # Preprocess the image
-        img = preprocess_image(img)
-
+        image = preprocess_image(image)
         # Make a prediction
-        pred = model.predict(img)
+        pred = model.predict(image)
         pred_label = 'Yawning' if pred[0][0] > 0.5 else 'No Yawning'
         pred_prob = pred[0][0]
-        
         # Show the prediction result
         st.write(f'Prediction: {pred_label}')
         st.write(f'Probability Of Yawning: {pred_prob:.2f}')
